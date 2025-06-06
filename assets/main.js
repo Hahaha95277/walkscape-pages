e2f77a2f2e138bae9174910ca27677069c655388
Hahaha95277 <108458445+Hahaha95277@users.noreply.github.com>
2025-06-06 20:31:38 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,22 @@
-// 無需 JavaScript，視差由 CSS perspective 實現
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.createElement("div");
+  container.className = "parallax-scene";
+
+  const far = document.createElement("img");
+  far.src = "./assets/glacier_far.jpg";
+  far.className = "parallax-layer far";
+
+  const mid = document.createElement("img");
+  mid.src = "./assets/glacier_mid.jpg";
+  mid.className = "parallax-layer mid";
+
+  const front = document.createElement("img");
+  front.src = "./assets/glacier_front.png";
+  front.className = "parallax-layer front";
+
+  container.appendChild(far);
+  container.appendChild(mid);
+  container.appendChild(front);
+
+  document.body.appendChild(container);
+});
